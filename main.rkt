5f04fc843e300897702854402a00269dd4680792
Matthew Flatt <mflatt@racket-lang.org>
2017-01-04 11:13:29 -0700

    fix sh command
    Use "> out 2>&1" instead of "&> out". Also, add some options.

--- main.rkt
+++ main.rkt
@@ -11,15 +11,19 @@
   (require racket/cmdline)
   
   (define beat-bucket #f)
+  (define log-verbose? #f)
 
   (command-line
    #:once-each
    [("--beat") bucket "Record heartbeat at <bucket>"
     (set! beat-bucket bucket)]
+   [("-v") "Log `raco setup -v`"
+    (set! log-verbose? #t)]
    #:args
-   (bucket)
+   ([bucket #f])
    (build bucket
-          #:beat-bucket beat-bucket)))
+          #:beat-bucket beat-bucket
+          #:log-verbose? log-verbose?)))
 
 (define (system! s)
   (printf "~a\n" s)
@@ -28,6 +32,7 @@
     (error "failed")))
 
 (define (build bucket
+               #:log-verbose? [log-verbose? #f]
                #:work-dir [work-dir (current-directory)]
                #:beat-bucket [beat-bucket #f]
                #:beat-task-name [beat-task-name "build-plot"])
@@ -45,11 +50,15 @@
       (system! "racket/bin/raco pkg config --set download-cache-max-files 10240")
       (system! "racket/bin/raco pkg config --set download-cache-max-bytes 671088640")
       
-      (system! "env PLTSTDERR=\"debug@GC error\" make CPUS=1 &> ../build-log.txt"))
+      (system! (string-append
+                "env PLTSTDERR=\"debug@GC error\" make CPUS=1"
+                (if log-verbose? " PLT_SETUP_OPTIONS=-v" "")
+                " > ../build-log.txt 2>&1")))
     
     (read-and-plot (list "build-log.txt") #f #t)
     
-    (upload bucket)
+    (when bucket
+      (upload bucket))
     (flush-output)
 
     (when beat-bucket
