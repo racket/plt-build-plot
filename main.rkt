0b249a2b61da6affa9592671ee8a9e5d26751aed
Matthew Flatt <mflatt@racket-lang.org>
2018-07-03 16:40:04 -0600

    add some building options, especially for CS

--- main.rkt
+++ main.rkt
@@ -12,7 +12,10 @@
   
   (define beat-bucket #f)
   (define log-verbose? #f)
+  (define skip-clean? #f)
   (define src-catalog #f)
+  (define variant #f)
+  (define racket #f)
 
   (command-line
    #:once-each
@@ -20,14 +23,23 @@
     (set! beat-bucket bucket)]
    [("-v") "Log `raco setup -v`"
     (set! log-verbose? #t)]
+   [("--skip-clean") "Skip the `git clean` step"
+    (set! skip-clean? #t)]
    [("--catalog") catalog "Set the source catalog"
     (set! src-catalog catalog)]
+   [("--variant") str "Choose a variant, such as `cs`"
+    (set! variant str)]
+   [("--racket") exec "Choose existing `racket` to drive the build"
+    (set! racket exec)]
    #:args
    ([bucket #f])
    (build bucket
           #:beat-bucket beat-bucket
           #:log-verbose? log-verbose?
-          #:catalog src-catalog)))
+          #:skip-clean? skip-clean?
+          #:catalog src-catalog
+          #:variant variant
+          #:racket racket)))
 
 (define (system! s)
   (printf "~a\n" s)
@@ -37,26 +49,42 @@
 
 (define (build bucket
                #:log-verbose? [log-verbose? #f]
+               #:skip-clean? [skip-clean? #f]
                #:work-dir [work-dir (current-directory)]
                #:beat-bucket [beat-bucket #f]
                #:beat-task-name [beat-task-name "build-plot"]
-               #:catalog [src-catalog #f])
+               #:catalog [src-catalog #f]
+               #:variant [variant #f] ; can be "cs"
+               #:racket [racket #f])  ; can be a path for RACKET=... to makefile
+  (define (variant-of s)
+    (if variant
+        (string-append variant "-" s)
+        s))
+  (define config (if racket
+                     (format "RACKET=~a" racket)
+                     ""))
+  (define GC-topic (if (equal? variant "cs")
+                       "GC:major"
+                       "GC"))
+  
   (define plt-dir (build-path work-dir "plt"))
   (parameterize ([current-directory work-dir])
     (unless (directory-exists? plt-dir)
       (system! "git clone https://github.com/racket/racket plt"))
     
     (parameterize ([current-directory plt-dir])
-      (system! "git clean -d -x -f")
+      (unless skip-clean?
+        (system! "git clean -d -x -f"))
       (system! "git pull")
       
-      (system! (format "make -j ~a base" (processor-count)))
+      (system! (format "make -j ~a ~a ~a" (processor-count) (variant-of "base") config))
       
-      (system! "racket/bin/raco pkg config --set download-cache-max-files 10240")
-      (system! "racket/bin/raco pkg config --set download-cache-max-bytes 671088640")
+      (system! (format "racket/bin/raco~a pkg config --set download-cache-max-files 10240" (or variant "")))
+      (system! (format "racket/bin/raco~a pkg config --set download-cache-max-bytes 671088640" (or variant "")))
       
       (system! (string-append
-                "env PLTSTDERR=\"debug@GC error\" make CPUS=1"
+                "env PLTSTDERR=\"debug@" GC-topic " error\" make " (variant-of "in-place")
+                " CPUS=1 " config
                 (if log-verbose? " PLT_SETUP_OPTIONS=-v" "")
                 (if src-catalog (format " SRC_CATALOG=~s" src-catalog) "")
                 " > ../build-log.txt 2>&1")))
